68b609b0591983c775880d1e0fab30a67fba4140
Eldos1337 <eldos.kulsharipov@gmail.com>
2024-11-17 21:32:48 +0500

    config.py

--- config.py
+++ config.py
@@ -2,15 +2,16 @@ import os
 
 SECRET_KEY = os.environ.get('APP_SECRET_KEY')
 
-DB_HOST = os.environ.get('DB_HOST', 'localhost')
-DB_USER = os.environ.get('DB_USER', 'root')
-DB_PASSWORD = os.environ.get('DB_PASSWORD', '')
-DB_DATABASE = os.environ.get('DB_DATABASE', '')
+DB_HOST = os.environ.get('DB_HOST', 'shopNA.mysql.pythonanywhere-services.com')
+DB_USER = os.environ.get('DB_USER', 'shopNA')
+DB_PASSWORD = os.environ.get('DB_PASSWORD', 'password000')
+DB_DATABASE = os.environ.get('DB_DATABASE', 'shopNA$back')
 
 
 db_config = {
-    'host': DB_HOST,
-    'user': DB_USER,
-    'password': DB_PASSWORD,
-    'database': DB_DATABASE
+    'host': 'shopNA.mysql.pythonanywhere-services.com',
+    'user': 'shopNA',
+    'password': 'password000',
+    'database': 'shopNA$back',
+    'auth_plugin': 'mysql_native_password'
 }
